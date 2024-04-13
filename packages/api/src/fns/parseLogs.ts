919648325c796c592ad0680d57eb5273299ea36a
Cristóvão Honorato <cristovao.honorato@gmail.com>
2024-04-13 16:54:33 +0200

    parseLogs function

--- packages/api/src/fns/parseLogs.ts
+++ packages/api/src/fns/parseLogs.ts
@@ -1,12 +1,20 @@
 import assert from 'assert'
 import {
-  Hash,
-  Log,
-  Hex,
+  Address,
+  Block,
   decodeAbiParameters,
-  parseAbiParameters,
+  encodeAbiParameters,
   getAddress,
+  Hash,
+  Hex,
+  keccak256,
+  Log,
+  pad,
+  parseAbiParameters,
+  toHex,
 } from 'viem'
+import { RegistryV2Event } from '@prisma/client'
+
 import {
   isDelegationCleared,
   isDelegationUpdated,
@@ -14,7 +22,40 @@ import {
   isOptOut,
 } from './logTopics'
 
-export default function parseLogs(logs: Log[]) {}
+export default function parseLogs(
+  entries: { chainId: number; block: Block; log: Log }[]
+): RegistryV2Event[] {
+  return entries.map(({ chainId, block, log }) => {
+    const { account, space } = parse(log)
+    assert(typeof log.transactionIndex == 'number')
+    assert(typeof log.logIndex == 'number')
+    return withEventId({
+      chainId,
+      registry: log.address,
+      blockNumber: Number(block.number),
+      blockTimestamp: Number(block.timestamp),
+      transactionIndex: log.transactionIndex,
+      logIndex: log.logIndex,
+      space,
+      account,
+      topics: log.topics,
+      data: log.data,
+    })
+  })
+}
+
+function parse({ topics, data }: { topics: string[]; data: string }) {
+  if (isDelegationUpdated({ topics, data })) {
+    return parseDelegationUpdated({ topics, data })
+  }
+  if (isDelegationCleared({ topics, data })) {
+    return parseDelegationCleared({ topics, data })
+  }
+  if (isExpirationUpdated({ topics, data })) {
+    return parseExpirationUpdated({ topics, data })
+  }
+  return parseOptOut({ topics, data })
+}
 
 export function parseDelegationUpdated({
   topics,
@@ -114,5 +155,55 @@ export function parseOptOut({
     account,
     space: context,
     optOut,
+  }
+}
+
+export function withEventId(
+  event: Omit<RegistryV2Event, 'id'>
+): RegistryV2Event {
+  const {
+    chainId,
+    registry,
+    blockNumber,
+    blockTimestamp,
+    transactionIndex,
+    logIndex,
+    space,
+    account,
+    topics,
+    data,
+  } = event
+
+  const hash = keccak256(
+    encodeAbiParameters(
+      [
+        { type: 'bytes32' },
+        { type: 'bytes32' },
+        { type: 'bytes32' },
+        { type: 'bytes32' },
+        { type: 'bytes32' },
+        { type: 'bytes32' },
+        { type: 'string' },
+        { type: 'bytes32' },
+        { type: 'bytes32[]' },
+        { type: 'bytes' },
+      ],
+      [
+        pad(toHex(BigInt(chainId))),
+        pad(registry as Address),
+        pad(toHex(blockNumber)),
+        pad(toHex(blockTimestamp)),
+        pad(toHex(transactionIndex)),
+        pad(toHex(logIndex)),
+        space,
+        pad(account as Address),
+        topics as Hash[],
+        data as Hex,
+      ]
+    )
+  )
+  return {
+    ...event,
+    id: `${chainId}-${blockNumber}-${registry}--${hash.slice(2, 2 + 16)}`,
   }
 }
