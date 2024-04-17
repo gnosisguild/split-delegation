340578cc08268bd3c7807e034f45e19e54ab8aa4
Cristóvão Honorato <cristovao.honorato@gmail.com>
2024-04-17 17:35:01 +0200

    Including indexes in the schema

---
+++ packages/api/prisma/migrations/20240417153415_add_indexes/migration.sql
@@ -0,0 +1,5 @@
+-- CreateIndex
+CREATE INDEX "DelegationEvent_blockNumber_idx" ON "DelegationEvent"("blockNumber");
+
+-- CreateIndex
+CREATE INDEX "DelegationEvent_blockTimestamp_idx" ON "DelegationEvent"("blockTimestamp");
