0cd5cd917286e4330ac634d0758d2ea1f9dd6cdc
Cristóvão Honorato <cristovao.honorato@gmail.com>
2024-04-17 19:59:09 +0200

    Add first sketch of top route

--- packages/api/routes/v1/[space]/[tag]/delegates/top.ts
+++ packages/api/routes/v1/[space]/[tag]/delegates/top.ts
@@ -1,3 +1,16 @@
+import { BlockTag } from 'viem'
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+
+import spaceId from 'src/fns/spaceId'
+import parseRows from 'src/fns/parseRows'
+
+import top from 'src/actions/top'
+import { syncTip as sync } from 'src/commands/sync'
+
+import setCORS from 'routes/setCORS'
+
+import prisma from '../../../../../prisma/singleton'
+
 // /api/v1/safe.ggtest.eth/latest/delegates/top
 
 // response {
@@ -24,12 +37,35 @@ type TopResponse = {
 type DelegateResponse = {
   address: string
   delegatorCount: number
-  percentOfDelegators: number
-  percentOfVotes: number
-  votingPower: number
+  // TODO percentOfDelegators: number
+  // TODO percentOfVotes: number
+  // TODO votingPower: number
 }
 
-export const GET = async (
-  req: NextApiRequest,
-  res: NextApiResponse<TopResponse>
-) => {}
+export const GET = async (req: VercelRequest, res: VercelResponse) => {
+  if (setCORS(req, res)) return
+
+  const space = req.query.space as string
+  const tag = req.query.space as BlockTag
+
+  // weight caching will come in here
+
+  await sync(tag)
+
+  const actions = parseRows(
+    await prisma.delegationEvent.findMany({
+      where: { spaceId: spaceId(space) },
+      orderBy: { blockTimestamp: 'asc' },
+    })
+  )
+
+  const result = await top(actions, Date.now(), {
+    limit: 20,
+    offset: 0,
+    orderBy: 'count',
+  })
+
+  const response = { delegates: result }
+
+  res.json(response)
+}
