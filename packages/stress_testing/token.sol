dc4ed9fd9cc47b252e545ed95a482e0fc60195fa
samepant <sampanter@gmail.com>
2024-04-29 15:30:50 -0400

    remove stress-test dir

--- packages/stress_testing/token.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract MyDao is ERC20 {
-    constructor() ERC20("MyDao", "MD") {}
-}
