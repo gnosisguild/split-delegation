2a87ef8d381660d91791c796df48e864416e0510
samepant <sampanter@gmail.com>
2024-05-07 18:22:12 -0400

    add pin to cron action

--- packages/api/api/v1/sync.ts
+++ packages/api/api/v1/sync.ts
@@ -1,9 +1,15 @@
 import { RequestContext } from '@vercel/edge'
 import type { VercelRequest } from '@vercel/node'
 import sync from '../../src/commands/sync'
+import pin from 'src/commands/pin'
+
+const actions = async () => {
+  await sync()
+  await pin()
+}
 
 export const GET = async (req: VercelRequest, context: RequestContext) => {
-  context.waitUntil(sync())
+  context.waitUntil(actions())
 
   return new Response('Syncing...')
 }
