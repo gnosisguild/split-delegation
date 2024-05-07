254f4ecffc1216701bf32158acdd1ef5a622a9db
Cristóvão Honorato <cristovao.honorato@gmail.com>
2024-05-07 12:05:40 +0200

    Include patching mechanism to load leaner allocations file for safe-vested

---
+++ packages/api/src/loaders/loadRawScores.ts
@@ -0,0 +1,174 @@
+import assert from 'assert'
+import { Address, Chain, getAddress } from 'viem'
+import snapshot from '@snapshot-labs/snapshot.js'
+
+import { merge } from '../fns/bag'
+import { Scores } from 'src/types'
+import { timerEnd, timerStart } from 'src/fns/timer'
+
+export default async function loadRawScores({
+  chain,
+  blockNumber,
+  space,
+  strategies,
+  addresses,
+}: {
+  chain: Chain
+  blockNumber: number
+  space: string
+  strategies: any[]
+  addresses: Address[]
+}) {
+  const results = await Promise.all(
+    strategies.map((strategy) =>
+      loadStrategy({
+        chain,
+        blockNumber,
+        space,
+        strategy: maybePatchStrategy(strategy),
+        addresses,
+      })
+    )
+  )
+
+  return merge(...results)
+}
+
+async function loadStrategy({
+  chain,
+  blockNumber,
+  space,
+  strategy,
+  addresses,
+}: {
+  chain: Chain
+  blockNumber: number
+  space: string
+  strategy: any
+  addresses: Address[]
+}): Promise<Scores> {
+  const CHUNK = chunkForStrategy(strategy)
+
+  let result = {}
+  while (addresses.length) {
+    const start = timerStart()
+    const curr = addresses.slice(0, CHUNK)
+    console.log(`[Load Scores] ${space} ${strategy.name} ${curr.length}, start`)
+    result = {
+      ...result,
+      ...(await loadStrategyWithRetry({
+        chain,
+        blockNumber,
+        space,
+        strategy,
+        addresses: curr,
+      })),
+    }
+    console.log(
+      `[Load Scores] ${space} ${strategy.name} ${curr.length}, done in ${timerEnd(start)}ms`
+    )
+    addresses = addresses.slice(CHUNK)
+  }
+
+  return result
+}
+
+async function loadStrategyWithRetry({
+  chain,
+  blockNumber,
+  space,
+  strategy,
+  addresses,
+}: {
+  chain: Chain
+  blockNumber: number
+  space: string
+  strategy: any
+  addresses: Address[]
+}): Promise<Scores> {
+  try {
+    const [result] = await snapshot.utils.getScores(
+      space,
+      [strategy],
+      String(chain.id),
+      addresses,
+      blockNumber
+    )
+
+    assert(
+      Object.keys(result)
+        .slice(0, 50) // just a sample
+        .every((address) => address == getAddress(address)),
+      'snapshot.getScores Non Check-summed Address'
+    )
+    return result
+  } catch (e) {
+    if (addresses.length < 100) {
+      throw e
+    }
+
+    console.log(
+      `[Load Scores] ${space} ${strategy.name} ${addresses.length}, retry`
+    )
+
+    const args = {
+      chain,
+      blockNumber,
+      space,
+      strategy,
+    }
+
+    const middle = addresses.length / 2
+    const [a, b] = await Promise.all([
+      loadStrategyWithRetry({
+        ...args,
+        addresses: addresses.slice(0, middle),
+      }),
+      loadStrategyWithRetry({
+        ...args,
+        addresses: addresses.slice(middle),
+      }),
+    ])
+
+    return merge(a, b)
+  }
+}
+
+function chunkForStrategy(strategy: any): number {
+  if (strategy.name == 'safe-vested') {
+    return 5000
+  } else {
+    return 10000
+  }
+}
+
+function maybePatchStrategy(strategy: any) {
+  // {
+  //   name: 'safe-vested',
+  //   network: '1',
+  //   params: {
+  //     symbol: 'SAFE (vested)',
+  //     claimDateLimit: '2022-12-27T10:00:00+00:00',
+  //     allocationsSource:
+  //       'https://safe-claiming-app-data.safe.global/allocations/1/snapshot-allocations-data.json',
+  //   },
+  // }
+
+  const shouldPatch =
+    strategy?.name == 'safe-vested' &&
+    strategy?.params?.allocationsSource ==
+      'https://safe-claiming-app-data.safe.global/allocations/1/snapshot-allocations-data.json'
+
+  if (!shouldPatch) {
+    return strategy
+  }
+
+  return {
+    ...strategy,
+    params: {
+      ...strategy.params,
+      allocationsSource:
+        'https://raw.githubusercontent.com/gnosisguild/split-delegation/main/packages/api/api/v1/safe.eth/snapshot-allocations-trimmed.json',
+    },
+  }
+}
--- packages/api/src/loaders/loadScores.ts
+++ packages/api/src/loaders/loadScores.ts
@@ -1,13 +1,11 @@
-import async from 'async'
-import { Address, Chain, getAddress, keccak256, toBytes } from 'viem'
-import snapshot from '@snapshot-labs/snapshot.js'
+import { Address, Chain, keccak256, toBytes } from 'viem'
 
 import { timerEnd, timerStart } from '../../src/fns/timer'
-import { merge } from '../fns/bag'
 
 import { Scores } from '../types'
 
 import prisma from '../../prisma/singleton'
+import loadRawScores from './loadRawScores'
 
 export default async function loadScores({
   chain,
@@ -30,7 +28,7 @@ export default async function loadScores({
     strategies,
     addresses,
   })
-  console.log(`[Load Scores ] ${space}, done in ${timerEnd(start)}ms`)
+  console.log(`[Load Scores] ${space}, done in ${timerEnd(start)}ms`)
   return { scores }
 }
 
@@ -55,7 +53,7 @@ async function _load({
   })
 
   const { scores } = await cacheGet(key)
-  const missing = []
+  const missing: Address[] = []
   for (const address of addresses) {
     if (typeof scores[address] != 'number') {
       missing.push(address)
@@ -67,12 +65,12 @@ async function _load({
     console.log(`[Load Scores ] missing ${missing.length} entries`)
     allScores = {
       ...scores,
-      ...(await _loadScores({
+      ...(await loadRawScores({
         chain,
         blockNumber,
         space,
         strategies,
-        addresses,
+        addresses: missing,
       })),
     }
     await cachePut(key, { scores: allScores })
@@ -111,7 +109,7 @@ function cacheKey({
 async function cacheGet(key: string): Promise<{ scores: Scores }> {
   const hit = await prisma.cache.findFirst({ where: { key } })
   if (hit) {
-    console.log(`[Load Scores ] Cache Hit ${key}`)
+    console.log(`[Load Scores] Cache Hit ${key}`)
     return JSON.parse(hit.value)
   }
   return { scores: {} }
@@ -124,79 +122,5 @@ async function cachePut(key: string, { scores }: { scores: Scores }) {
     create: { key, value },
     update: { key, value },
   })
-  console.log(`[Load Scores ] Cache Put ${key}`)
-}
-
-async function _loadScores({
-  chain,
-  blockNumber,
-  space,
-  strategies,
-  addresses,
-}: {
-  chain: Chain
-  blockNumber: number
-  space: string
-  strategies: any[]
-  addresses: Address[]
-}): Promise<Record<Address, number>> {
-  const result = (await getScores({
-    chain,
-    blockNumber,
-    space,
-    strategies,
-    addresses,
-  })) as Record<Address, number>[]
-
-  const bag = merge(...result)
-
-  return Object.fromEntries(
-    addresses.map((address) => [getAddress(address), bag[address] || 0])
-  )
-}
-
-const CHUNK_SIZE = 500
-// const PARALLELISM = 5
-
-async function getScores({
-  chain,
-  blockNumber,
-  space,
-  strategies,
-  addresses,
-}: {
-  chain: Chain
-  blockNumber: number
-  space: string
-  strategies: any[]
-  addresses: Address[]
-}) {
-  return (
-    await async.mapSeries(
-      chunks(addresses, CHUNK_SIZE),
-      // PARALLELISM,
-      function (
-        addresses: string[],
-        done: (err: any, result: Record<Address, number>) => void
-      ) {
-        snapshot.utils
-          .getScores(
-            space,
-            strategies,
-            String(chain.id),
-            addresses,
-            blockNumber
-          )
-          .then((result) => done(null, result as Record<Address, number>))
-      }
-    )
-  ).flat()
-}
-
-function chunks<T>(array: T[], chunkSize: number): T[][] {
-  const result: T[][] = []
-  for (let i = 0; i < array.length; i += chunkSize) {
-    result.push(array.slice(i, i + chunkSize))
-  }
-  return result
+  console.log(`[Load Scores] Cache Put ${key}`)
 }
