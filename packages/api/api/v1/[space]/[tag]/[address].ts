78b8e899ee5f9ecc78ff935a31c4b4c4e40432a4
samepant <sampanter@gmail.com>
2024-05-10 12:14:45 -0400

    address stub

---
+++ packages/api/api/v1/[space]/[tag]/[address].ts
@@ -0,0 +1,25 @@
+// {
+//     "chainId": 1,
+//     "blockNumber": 19840583,
+//     "address": "0x485E60C486671E932fd9C53d4110cdEab1E7F0eb",
+//     "delegatorCount": 0,
+//     "percentOfDelegators": 0,
+//     "votingPower": 0,
+//     "percentOfVotingPower": 0,
+//     "delegates": [
+//         {
+//             "address": "0x37F1eE65C2F8610741cd9Dff1057F926809C4078",
+//             "direct": true,
+//             "delegatedPercent": "50", // how much weight this address gave to this delegate
+//             "delegatedPower": 21.715405067696775, // how much voting power this address gave to this delegate
+//         }
+//     ],
+//     "delegators": [
+//         {
+//             "address": "0x37F1eE65C2F8610741cd9Dff1057F926809C4078",
+//             "direct": true,
+//             "delegatedPercent": "50", // how much weight given to this address
+//             "delegatedPower": 21.715405067696775, // how much voting power given to this address
+//         }
+//     ]
+// }
