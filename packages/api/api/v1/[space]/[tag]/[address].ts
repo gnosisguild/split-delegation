84bc6f91d8bb3e061a93f65b273a66b6f1779fe5
Cristóvão Honorato <cristovao.honorato@gmail.com>
2024-05-12 19:43:44 +0200

    Serve address information as unified route

--- packages/api/api/v1/[space]/[tag]/[address].ts
+++ packages/api/api/v1/[space]/[tag]/[address].ts
@@ -1,3 +1,68 @@
+import { Address, BlockTag, getAddress } from 'viem'
+
+import delegateStream from '../../../../src/calculations/delegateStream'
+import delegatorStream from '../../../../src/calculations/delegatorStream'
+import kahn from '../../../../src/fns/graph/sort'
+
+import loadScores from '../../../../src/loaders/loadScores'
+import loadWeights from 'src/loaders/loadWeights'
+import resolveBlockTag from '../../../../src/loaders/resolveBlockTag'
+
+import { syncTip } from '../../../../src/commands/sync'
+
+import { DelegatorRequestBody } from '../../../../src/types'
+
+export const POST = async (req: Request) => {
+  const searchParams = new URL(req.url || '').searchParams
+  const space = searchParams.get('space') as string
+  const tag = searchParams.get('tag') as BlockTag
+  const address = getAddress(searchParams.get('address') as string)
+
+  const { strategies, network, totalSupply } =
+    (await req.json()) as DelegatorRequestBody
+
+  const { chain, blockNumber } = await resolveBlockTag(tag, network)
+
+  await syncTip(chain, blockNumber)
+
+  const { weights } = await loadWeights({
+    chain,
+    blockNumber,
+    space,
+  })
+  const order = kahn(weights) as Address[]
+
+  const { scores } = await loadScores({
+    chain,
+    blockNumber,
+    space,
+    strategies,
+    addresses: order.includes(address) ? order : [...order, address],
+  })
+
+  const response = {
+    chainId: chain.id,
+    blockNumber,
+    address,
+    delegators: delegatorStream({
+      weights,
+      scores,
+      order,
+      toDelegate: address,
+    }),
+    delegates: delegateStream({
+      weights,
+      score: scores[address]!,
+      order,
+      fromDelegator: address,
+    }),
+  }
+
+  return new Response(JSON.stringify(response), {
+    headers: { 'Content-Type': 'application/json' },
+  })
+}
+
 // {
 //     "chainId": 1,
 //     "blockNumber": 19840583,
