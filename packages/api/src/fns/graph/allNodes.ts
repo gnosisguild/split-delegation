2d2156023e2f9ca2e6a433c59b23946484f08672
Cristóvão Honorato <cristovao.honorato@gmail.com>
2024-05-18 20:15:36 +0200

    bring back all nodes function

---
+++ packages/api/src/fns/graph/allNodes.ts
@@ -0,0 +1,13 @@
+import { Graph } from '../../types'
+
+export default function allNodes(weights: Graph): string[] {
+  const set = new Set<string>()
+  for (const delegator of Object.keys(weights)) {
+    set.add(delegator)
+
+    for (const delegate of Object.keys(weights[delegator])) {
+      set.add(delegate)
+    }
+  }
+  return Array.from(set).sort()
+}
