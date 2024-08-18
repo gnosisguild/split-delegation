e6b607e415c80c9445c847c484f7a4d6a7d5ef4f
Cristóvão Honorato <cristovao.honorato@gmail.com>
2024-08-18 19:57:23 +0200

    feat: in addition to including expiration on the delegator and delegate trees, also include it in the address stats

--- packages/api/src/calculations/addressStats.ts
+++ packages/api/src/calculations/addressStats.ts
@@ -1,3 +1,4 @@
+import assert from 'assert'
 import basisPoints from '../fns/basisPoints'
 import calculateVotingPower from './votingPower'
 import reachable from '../fns/graph/reachable'
@@ -27,6 +28,7 @@ export default function addressStats({
 
   return {
     address,
+    expiration: extractExpiration({ delegations, address }),
     votingPower,
     incomingPower,
     outgoingPower,
@@ -34,4 +36,28 @@ export default function addressStats({
     delegatorCount,
     percentOfDelegators: basisPoints(delegatorCount, allDelegatorCount),
   }
+}
+
+function extractExpiration({
+  delegations,
+  address,
+}: {
+  delegations: DelegationDAGs
+  address: string
+}): number {
+  if (!delegations.forward[address]) {
+    return 0
+  }
+
+  const expirations = new Set(
+    Object.values(delegations.forward[address]).map(
+      ({ expiration }) => expiration
+    )
+  )
+
+  // Ensure that all expirations are the same, if there are any. Should never happen
+  assert(expirations.size <= 1)
+
+  // Return the single expiration value, if it exists, otherwise return 0
+  return expirations.size === 1 ? Array.from(expirations)[0] : 0
 }
