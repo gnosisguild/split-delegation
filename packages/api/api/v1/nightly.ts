e7e3c3c0890cc343fcefef3410dab663763753dd
Cristóvão Honorato <cristovao.honorato@gmail.com>
2025-01-16 12:27:45 +0100

    chore: rebase cleanup

--- packages/api/api/v1/nightly.ts
+++ packages/api/api/v1/nightly.ts
@@ -1,16 +1,21 @@
 import { RequestContext } from '@vercel/edge'
 import type { VercelRequest } from '@vercel/node'
-import prune from '../../src/commands/prune'
+
+import { cachePrune } from '../../src/loaders/cache'
 
 const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
 
 const actions = async () => {
   await wait(5000)
-  await prune()
+  await cachePrune(oneDayAgo())
 }
 
 export const GET = async (req: VercelRequest, context: RequestContext) => {
   context.waitUntil(actions())
 
   return new Response('Syncing...')
+}
+
+function oneDayAgo() {
+  return new Date(Date.now() - 24 * 60 * 60 * 1000)
 }
--- packages/api/src/commands/prune.ts
+++
@@ -1,9 +0,0 @@
-import { cachePrune } from '../loaders/cache'
-
-export default async function () {
-  await cachePrune(oneDayAgo())
-}
-
-function oneDayAgo() {
-  return new Date(Date.now() - 24 * 60 * 60 * 1000)
-}
