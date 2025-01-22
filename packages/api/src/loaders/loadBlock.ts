2a2736736a080baa9bfb060c72cb58ebbea1464d
Cristóvão Honorato <cristovao.honorato@gmail.com>
2025-01-22 10:25:19 +0100

    refact: improve logging in loadBlock

--- packages/api/src/loaders/loadBlock.ts
+++ packages/api/src/loaders/loadBlock.ts
@@ -1,47 +1,61 @@
 import assert from 'assert'
 import { BlockTag, Chain, keccak256, toBytes } from 'viem'
 
+import { timerEnd, timerStart } from '../fns/timer'
+
 import { cacheGet, cachePut } from './cache'
 import createClient from './createClient'
+
+type Result = { blockNumber: number; blockTimestamp: number }
 
 const LOG_PREFIX = 'LoadBlock'
 
 export default async function loadBlock(
   chain: Chain,
   blockTag: BlockTag | number
-): Promise<{ blockNumber: number; blockTimestamp: number }> {
-  {
-    const entry: { blockNumber: number; blockTimestamp: number } | undefined =
-      isBlockNumber(blockTag)
-        ? await cacheGet(
-            cacheKey({ chain, blockNumber: Number(blockTag) }),
-            LOG_PREFIX
-          )
-        : undefined
+): Promise<Result> {
+  const start = timerStart()
 
-    if (entry) {
-      return entry
+  const { blockNumber, blockTimestamp } = await _load(chain, blockTag)
+
+  console.log(
+    `[${LOG_PREFIX}] ${blockNumber} @ ${chain.name}, done in ${timerEnd(start)}ms`
+  )
+  return { blockNumber, blockTimestamp }
+}
+
+async function _load(
+  chain: Chain,
+  blockTag: BlockTag | number
+): Promise<Result> {
+  let result = isBlockNumber(blockTag)
+    ? await cacheGet(
+        cacheKey({ chain, blockNumber: Number(blockTag) }),
+        LOG_PREFIX
+      )
+    : null
+
+  if (!result) {
+    const block = await createClient(chain).getBlock({
+      ...(isBlockNumber(blockTag)
+        ? { blockNumber: BigInt(blockTag) }
+        : { blockTag: blockTag as BlockTag }),
+      includeTransactions: false,
+    })
+
+    result = {
+      blockNumber: Number(block.number),
+      blockTimestamp: Number(block.timestamp),
     }
+
+    await cachePut(
+      cacheKey({ chain, blockNumber: Number(block.number) }),
+      { chainId: chain.id, ...result },
+      LOG_PREFIX
+    )
   }
 
-  console.log(`[${LOG_PREFIX}] Miss ${blockTag} @ ${chain.name}`)
-  const block = await createClient(chain).getBlock({
-    ...(isBlockNumber(blockTag)
-      ? { blockNumber: BigInt(blockTag) }
-      : { blockTag: blockTag as BlockTag }),
-    includeTransactions: false,
-  })
-
-  const key = cacheKey({ chain, blockNumber: Number(block.number) })
-  const entry = {
-    chainId: chain.id,
-    blockNumber: Number(block.number),
-    blockTimestamp: Number(block.timestamp),
-  }
-
-  await cachePut(key, entry, LOG_PREFIX)
-
-  return entry
+  return result
 }
 
 function cacheKey({
