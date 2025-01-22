31d96d11f6cf0e334357e6004b991568de87912a
Cristóvão Honorato <cristovao.honorato@gmail.com>
2025-01-22 10:58:28 +0100

    refact: improve logging in loadTopDelegates

--- packages/api/src/loaders/loadTopDelegates.ts
+++ packages/api/src/loaders/loadTopDelegates.ts
@@ -27,30 +27,17 @@ export default async function loadTopDelegates({
   totalSupply: number
 }): Promise<{ topDelegates: DelegateStats[] }> {
   const start = timerStart()
-  const key = cacheKey({ chain, blockNumber, space, strategies, totalSupply })
 
-  let entry: { topDelegates: DelegateStats[] } | null = await cacheGet(
-    key,
-    LOG_PREFIX
-  )
-  if (!entry) {
-    entry = await _load({
-      chain,
-      blockNumber,
-      space,
-      strategies,
-      totalSupply,
-    })
-    await cachePut(
-      key,
-      { chain: chain.id, blockNumber, space, topDelegates: entry.topDelegates },
-      LOG_PREFIX
-    )
-  }
-
+  const result = await _load({
+    chain,
+    blockNumber,
+    space,
+    strategies,
+    totalSupply,
+  })
   console.log(`[${LOG_PREFIX}] ${space}, done in ${timerEnd(start)}ms`)
 
-  return entry
+  return result
 }
 
 async function _load({
@@ -65,7 +52,17 @@ async function _load({
   space: string
   strategies: any[]
   totalSupply: number
-}) {
+}): Promise<{ topDelegates: DelegateStats[] }> {
+  const key = cacheKey({ chain, blockNumber, space, strategies, totalSupply })
+
+  const entry: { topDelegates: DelegateStats[] } | null = await cacheGet(
+    key,
+    LOG_PREFIX
+  )
+  if (entry) {
+    return { topDelegates: entry.topDelegates }
+  }
+
   const dags = await loadDelegationDAGs({
     chain,
     blockNumber,
@@ -80,15 +77,21 @@ async function _load({
     addresses: allNodes(dags.forward),
   })
 
-  return {
-    topDelegates: top(
-      delegateStats({
-        dags,
-        scores,
-        totalSupply,
-      })
-    ),
-  }
+  const topDelegates: DelegateStats[] = top(
+    delegateStats({
+      dags,
+      scores,
+      totalSupply,
+    })
+  )
+
+  await cachePut(
+    key,
+    { chain: chain.id, blockNumber, space, topDelegates },
+    LOG_PREFIX
+  )
+
+  return { topDelegates }
 }
 
 function cacheKey({
